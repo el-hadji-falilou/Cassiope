74ea99743a3d4733fb49fa2c44c9641505ed45f8
el-hadji-falilou <nfallou622@gmail.com>
2025-03-17 02:25:03 +0100

    Conteneurs Docker

---
+++ TP -  Falilou/part1/Identity/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e  # Stopper l'exécution en cas d'erreur critique
+
+# Fichier de code de l'élève
+CODE_FILE="/app/minicipher.py"
+EXECUTABLE_FILE="/app/minicipher"
+
+# Vérifier si le fichier Python soumis existe
+if [ ! -f "$CODE_FILE" ]; then
+    echo "Aucun fichier minicipher.py trouvé. Assurez-vous de monter le fichier correctement."
+    exit 1
+fi
+
+echo "Création de l'exécutable avec Make..."
+
+# Exécuter la commande Make pour compiler l'exécutable
+make python
+
+# Vérifier si l'exécutable a été créé
+if [ ! -f "$EXECUTABLE_FILE" ]; then
+    echo "L'exécutable minicipher n'a pas été créé. Vérifiez la compilation."
+    exit 1
+fi
+
+echo "Lancement des tests via test-identity.sh..."
+
+# Exécuter le script de test avec l'argument `0` et affiche la sortie
+bash test-identity.sh 0
