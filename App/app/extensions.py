dfb41e43887116ec5cf4bfcfcb2235d4853c1083
EricoNgoa <164075015+EricoNgoa@users.noreply.github.com>
2025-06-03 23:26:18 +0200

    Update extensions.py

--- App/app/extensions.py
+++ App/app/extensions.py
@@ -1,9 +1,12 @@
+# Initialisation des extensions Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate    import Migrate
 from flask_login      import LoginManager
 from flask_mail       import Mail
 
-db      = SQLAlchemy()
-migrate = Migrate()
-login   = LoginManager()
-login.login_view = 'auth.login'
+# Instances des extensions
+db      = SQLAlchemy() # ORM pour la base de données
+migrate = Migrate()    # Gestion des migrations
+login   = LoginManager() # Gestion d'authentification
+login.login_view = 'auth.login' # Vue de login par défaut
+mail = Mail()  # Service d'envoi d'emails
