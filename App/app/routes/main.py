6d521e05d095126df59b7012985689140733e0b5
EricoNgoa <164075015+EricoNgoa@users.noreply.github.com>
2025-06-03 23:34:51 +0200

    Update main.py
    commentaire

--- App/app/routes/main.py
+++ App/app/routes/main.py
@@ -1,12 +1,11 @@
-# app/routes/main.py
+# Routes principales (accueil)
 from flask import Blueprint, render_template, redirect, url_for
 from flask_login import login_required, current_user
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-@login_required
+@login_required  # Accès protégé
 def index():
-    # Ici vous pouvez récupérer vos questions / steps si besoin,
-    # et le matériel crypto via current_user.cohort.materials
+  """Page d'accueil après connexion"""
     return render_template('index.html')
